6138a12d1545452241014644e68d17ecb34d8c55
Gurkirat Singh <gurkiratsingh2001@gmail.com>
2021-10-31 03:28:45 +0530

    wip

---
+++ questions/checking.py
@@ -0,0 +1,167 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import logging
+from optimizer import Optimizer
+
+logging.basicConfig(filename='output/run.log', level=logging.DEBUG)
+logger = logging.getLogger("Optimizer")
+
+get_next_pose = lambda initial, change: np.array(
+    [
+        initial[0] + change[0] * np.cos(initial[2]) - change[1] * np.sin(
+            initial[2]),
+        initial[1] + change[1] * np.cos(initial[2]) + change[0] * np.sin(
+            initial[2]),
+        initial[2] + change[2],
+    ]
+)
+
+initial_poses = []
+od_constraints = []
+loop_constraints = []
+
+data_file = open("../data/noisy.g2o")
+
+for line in data_file.readlines():
+    params = line[:-1].split(" ")
+
+    if len(params) < 5:
+        continue
+
+    if params[0] == "VERTEX_SE2":
+        initial_poses.append(
+            np.array([float(params[2]), float(params[3]), float(params[4])]))
+        # n * 3
+    else:
+        pose_start = int(params[1])
+        pose_end = int(params[2])
+
+        formatted_params = [
+            [
+                pose_start,
+                pose_end,
+            ],
+            [
+                float(params[3]),
+                float(params[4]),
+                float(params[5]),
+            ],
+        ]
+
+        if pose_end == pose_start + 1:
+            od_constraints.append(formatted_params)
+        else:
+            loop_constraints.append(formatted_params)
+
+initial_poses = np.array(initial_poses)
+
+constraints = []
+constraints.extend(od_constraints)
+constraints.extend(loop_constraints)
+
+n_poses = initial_poses.shape[0]
+n_constraints = len(constraints)
+
+
+def f(state):
+    poses = state.reshape(-1, 3)
+    fixed_pose = initial_poses[0]
+    f_array = []
+
+    for constraint in constraints:
+        pose_start = constraint[0][0]
+        pose_end = constraint[0][1]
+
+        diff = get_next_pose(poses[pose_start], constraint[1]) - poses[pose_end]
+        f_array.extend(diff)
+
+    # initial position
+    f_array.extend(poses[0] - fixed_pose)
+
+    f_array = np.array([f_array]).T
+
+    return f_array
+
+
+f_initial = f(initial_poses)
+
+state_vector = initial_poses.reshape(-1, 1)
+
+j_shape = (f_initial.shape[0], state_vector.shape[0])
+
+
+def jac(state: np.ndarray):
+    J = np.zeros(j_shape)
+    for i, constraint in enumerate(constraints):
+        pose_start = constraint[0][0]
+        pose_end = constraint[0][1]
+        theta_start = state[3 * pose_start + 2]
+
+        # x constraint
+        J[3 * i, 3 * pose_start] = 1
+        J[3 * i, 3 * pose_end] = -1
+        J[3 * i, 3 * pose_start + 2] = -constraint[1][0] * np.sin(theta_start) - \
+                                       constraint[1][1] * np.cos(theta_start)
+
+        # y constraint
+        J[3 * i + 1, 3 * pose_start + 1] = 1
+        J[3 * i + 1, 3 * pose_end + 1] = -1
+        J[3 * i + 1, 3 * pose_start + 2] = -constraint[1][1] * np.sin(
+            theta_start) + constraint[1][0] * np.cos(theta_start)
+
+        # theta constraint
+        J[3 * i + 2, 3 * pose_start + 2] = 1
+        J[3 * i + 2, 3 * pose_end + 2] = -1
+
+    # fixed poses
+    J[-3, 0] = 1
+    J[-2, 1] = 1
+    J[-1, 2] = 1
+    return J
+
+
+J = jac(state_vector)
+
+
+def draw(X, Y, THETA, name):
+    ax = plt.subplot(111)
+    ax.plot(X, Y, 'ro')
+    plt.plot(X, Y, 'c-')
+
+    for i in range(len(THETA)):
+        x2 = 0.25 * np.cos(THETA[i]) + X[i]
+        y2 = 0.25 * np.sin(THETA[i]) + Y[i]
+        plt.plot([X[i], x2], [Y[i], y2], 'g->')
+
+    plt.savefig(name)
+
+
+def main():
+    info_mat = np.eye(f_initial.shape[0])
+    values = [(500,700,1000)]
+    for od_info, loop_info, fix_info in values:
+        n_od = 3 * len(od_constraints)
+        n_loop = 3 * len(loop_constraints)
+
+        info_mat[:n_od, :n_od] = od_info * np.eye(n_od)
+        info_mat[n_od:n_od + n_loop, n_od:n_od + n_loop] = loop_info * np.eye(
+            n_loop)
+        info_mat[-3:, -3:] = fix_info * np.eye(3)
+        state = state_vector.copy()
+        solver = Optimizer(f, jac, state, info_mat, algo='LM', lm_lambda=1e-3)
+        print(solver.losses)
+        # plt.plot(solver.losses)
+        # plt.savefig(f"output/{fix_info}_{loop_info}_{od_info}_loss.png")
+
+        final_vector = solver.get_current()
+
+        poses = final_vector.reshape(-1, 3)
+        # draw(poses[:, 0], poses[:, 1], poses[:, 2],
+        #      f"output/{fix_info}_{loop_info}_{od_info}_path.png")
+        #
+        # logger.info(f"{fix_info}_{loop_info}_{od_info}: {solver.loss()}")
+
+
+
+if __name__ == '__main__':
+    main()
---
+++ questions/optimizer.py
@@ -0,0 +1,99 @@
+from typing import Callable
+import numpy as np
+import logging
+
+
+class Optimizer:
+    def __init__(self, f: Callable[[np.ndarray], np.ndarray],
+                 jac: Callable[[np.ndarray], np.ndarray],
+                 initial_values: np.ndarray, info_mat: np.ndarray, n_iter=10,
+                 algo='GN', lm_lambda=1e-3):
+        """
+        Optmizer for given loss_function and jacobial function
+        :param f:  Loss function
+        :param jac: Function to compute jac at a state
+        :param initial_values: Initial values of state
+        :param algo: 'GN' for gauss newton, 'LM' for LM
+        :param lm_lambda: Initial Lambda for LM
+
+        """
+
+        self.f = f
+        self.jac = jac
+        self.initial_values = initial_values
+        self.info_mat = info_mat
+        self.n_iter = n_iter
+        self.algo = algo
+        self.lm_lambda = lm_lambda
+        self.prev_loss = -1
+
+        self.F = lambda x: 0.5 * np.squeeze(f(x).T @ info_mat @ f(x))
+        self.__current = self.initial_values.copy()
+        self.__iter = 0
+
+        self.losses = [self.loss()]
+        print(f"INFO: Using ALGO: {algo}")
+        print(f"INFO Initial Error: {self.F(self.__current)}")
+
+    def get_current(self):
+        return self.__current
+
+    def get_gn_change(self) -> np.ndarray:
+        J = self.jac(self.__current)
+        H = J.T @ self.info_mat @ J
+        b = J.T @ self.info_mat.T @ self.f(self.__current)
+        dx = -np.linalg.inv(H) @ b
+        return dx
+
+    def get_lm_change(self) -> np.ndarray:
+        J = self.jac(self.__current)
+        H = J.T @ self.info_mat @ J
+        H += self.lm_lambda * np.eye(H.shape[0])
+        b = J.T @ self.info_mat.T @ self.f(self.__current)
+        dx = -np.linalg.inv(H) @ b
+
+        return dx
+
+    def print_info(self):
+        print(f"Iteration: {self.__iter} / {self.n_iter}")
+        print(f"Loss: {self.F(self.__current)}")
+        # TODO: Draw plots
+
+    def gn_update(self):
+        self.__iter += 1
+        dx = self.get_gn_change()
+        self.__current += dx
+        self.print_info()
+        return abs(self.loss() - self.prev_loss) < 1e-5
+
+    def lm_update(self):
+        self.__iter += 1
+        dx = self.get_lm_change()
+        prev_loss = self.F(self.__current)
+        new_current = self.__current + dx
+        if self.F(new_current) < prev_loss:
+            print("LM: Update Accepted")
+            self.__current += dx
+            self.lm_lambda /= 10
+            self.print_info()
+            return abs(self.loss() - self.prev_loss) < 1e-5
+        else:
+            print("LM: Update Rejected")
+            self.lm_lambda *= 10
+            return False
+
+    def loss(self):
+        return self.F(self.__current)
+
+    def optimize(self):
+        self.print_info()
+        for i in range(self.n_iter):
+            to_break = False
+            if self.algo == 'GN':
+                to_break = self.gn_update()
+            else:
+                to_break = self.lm_update()
+            if to_break:
+                break
+            self.losses.append(self.loss())
+            self.prev_loss = self.loss()
